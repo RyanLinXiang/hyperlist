c3a1d2c4eca722c652f2d1c8bfd1a914a9331f1f
Ryan Lin Xiang <Ryan.Lin.Xiang@gmail.com>
2020-05-26 00:32:54 +0200

    Merged with ricky

--- src/Components/api.js
+++ src/Components/api.js
@@ -1,8 +1,8 @@
 const connectAPI = async (extension, type, data = "", token = false) => {
   const url = "https://awacademy-kleinanzeigen.azurewebsites.net/" + extension;
 
-  console.log(type,extension, data)
-  
+  console.log(type, extension, data);
+
   const dataobj = {
     method: type,
     headers: {
@@ -10,21 +10,10 @@ const connectAPI = async (extension, type, data = "", token = false) => {
     },
   };
 
-<<<<<<< HEAD
   if (token) dataobj.headers["Authorization"] = `Bearer ${token}`;
 
   if (data) dataobj.body = JSON.stringify(data);
 
-||||||| merged common ancestors
-  if (data) {
-    dataobj.body = JSON.stringify(data);
-  }
-=======
-  if (data) {
-    dataobj.body = JSON.stringify(data);
-  }
-  console.log('Hello',dataobj)
->>>>>>> origin/ricky
   const response = await fetch(url, dataobj);
 
   return response.statusText === "No Content" ? {} : response.json();
