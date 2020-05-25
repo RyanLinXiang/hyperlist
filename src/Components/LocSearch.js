c77a6c9c0a474a7d3a33f6ac393dab24aef80300
osamaAlhaj <osama.alhaj.khaled@gmail.com>
2020-05-25 16:20:31 +0200

    search-done

--- src/Components/LocSearch.js
+++ src/Components/LocSearch.js
@@ -1,19 +1,55 @@
 import React from "react";
+import connectAPI from "./api";
 
-const LocSearch = () => {
+
+class LocSearch extends React.Component {
+  handlerSubmit= (e)=> {
+    e.preventDefault()
+    let keyWord = e.target.value
+    var locationRequest = {
+      limit: 20,
+      offset: 0,
+      where: {
+        and: [
+          { location: { like: keyWord, options: "i" } }
+
+
+        ]
+      }
+    };
+    
+   
+    this.props.handler("default", locationRequest)
+   
+  }
+   
+  Locations=()=>{
+   connectAPI("/ad", "GET").then(e =>{ 
+    let allLocations =[]
+    for (let i=0;i < e.length;i++) { 
+      allLocations.push(e[i].location)
+    } })
+    
+    
+  } 
+ 
+
+ 
+  render(){
   return (
-    <div className="menu sticky">
+    <div  className="menu sticky">
       <div className="control">
         <input
+          onChange= {this.handlerSubmit}
           className="input is-radiusless"
           type="text"
           id="search_loc"
           placeholder="Find location"
         />
-        <ul id="matches"> </ul>
+        
       </div>
     </div>
   );
-};
+}};
 
 export default LocSearch;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,25 +1,53 @@
 import React from "react";
+import connectAPI from "./api";
 
-const Search = () => {
-  return (
-    <div className="box is-shadowless">
-      <div className="field has-addons">
-        <p className="control is-expanded">
-          <input
-            className="input"
-            id="search"
-            type="text"
-            placeholder="Find ads"
-          />
-        </p>
-        <p className="control">
-          <button className="button is-link" id="search_submit">
-            Search
+
+class Search extends React.Component {
+
+  handlerSubmit= (e)=> {
+    console.log("Hello")
+    e.preventDefault()
+    let keyWord = e.target.search.value
+    var searchRequest = {
+      "limit": "20",
+      "offset": "0",
+      "where": {
+        "and": [
+          { "title": { "like": keyWord, "options": "i" } }
+
+
+        ]
+      }
+    };
+    console.log(searchRequest)
+    connectAPI("/ad?filter="+ encodeURIComponent(JSON.stringify(searchRequest)), "GET").then(e => {console.log(e);this.props.handler(e)})
+   
+  }
+
+
+  render() {
+    return (
+      <form onSubmit={this.handlerSubmit}>
+        <div className="box is-shadowless">
+          <div className="field has-addons">
+            <p className="control is-expanded">
+              <input
+                className="input"
+                id="search"
+                type="text"
+                placeholder="Find ads"
+              />
+            </p>
+            <p className="control">
+              <button type="submit"  className="button is-link" id="search_submit">
+                Search
           </button>
-        </p>
-      </div>
-    </div>
-  );
+            </p>
+          </div>
+        </div>
+      </form>
+    );
+  }
 };
 
 export default Search;
